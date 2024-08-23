47caa475a396fabcd2911235c0fe3f98694b8c59
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-08-23 17:47:45 +0900

    Update b.java

--- java_test/b.java
+++ java_test/b.java
@@ -2,9 +2,15 @@ package java_test;
 
 public class b {
     public static void main(String[] args) {
-
-        // 이 부분에 code를 작성해주세요!
-
+        Scanner scanner = new Scanner(System.in);
+        char ch = scanner.next().charAt(0);
+        ch = Character.toLowerCase(ch);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("O");
+        } else {
+            System.out.println("X");
+        }
+        scanner.close();
     }
 
 }
