345c7d93799c8434f4e94ff8868bdae424b595fc
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-08-23 17:48:48 +0900

    Update e.java

--- java_test/e.java
+++ java_test/e.java
@@ -2,9 +2,32 @@ package java_test;
 
 public class e {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int days = 0;
 
-        // 이 부분에 code를 작성해주세요!
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                days = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                days = 30;
+                break;
+            case 2:
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+                break;
+            default:
+                scanner.close();
+                return;
+        }
 
+        System.out.println(days);
+        scanner.close();
     }
 
 }
